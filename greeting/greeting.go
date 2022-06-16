1aeb8a4327514606e364df0f85efb044a6db575e
reda.ghoussoub@gmail.com <reda.ghoussoub@gmail.com>
2022-06-15 22:17:52 -0400

    initial commit

---
+++ greeting/greeting.go
@@ -0,0 +1,14 @@
+// Package greeting implements greeting outputs to console
+package greeting
+
+import "fmt"
+
+// Greeting outputs a greeting to the console
+func Greeting() {
+	fmt.Println("Greetings !!!")
+}
+
+// Hi outputs hi to the console
+func Hi() {
+	fmt.Println("Hi !!!")
+}
